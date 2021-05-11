a30e953b6b3dfb309fd0dd79f5f2be138b7f87a1
Yoshiaki Togami <tuabobo123@gmail.com>
2021-05-11 20:20:28 +0900

    enable to input multiple command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::io::stdin;
+use std::io::{stdout, Write};
 use std::process::Command;
+use std::result::Result;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let command = input.trim();
+    loop {
+        print!("> ");
+        stdout().flush();
 
-    Command::new(command).spawn().unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+        child.wait();
+    }
 }
